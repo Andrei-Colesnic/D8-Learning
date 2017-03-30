e221c6cb5dc04583330ba15cd3508b78d6e4091a
Andrei-Colesnic <the.tai.pen@gmail.com>
2017-03-30 10:19:38 +0300

    Improve cutom service for Node View count

--- modules/custom/view_count/src/GetViewDataService.php
+++ modules/custom/view_count/src/GetViewDataService.php
@@ -7,18 +7,21 @@
 
 namespace Drupal\view_count;
 
+
+use Drupal\Core\Database\Connection;
+
 class GetViewDataService {
 
   protected $all_views;
-  protected $today_views;
   protected $page_view;
+  protected $connection;
 
 
   /**
    * When the service is created, set a value for the example variable.
    */
-  public function __construct() {
-
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
   }
 
   /**
@@ -27,14 +30,14 @@ class GetViewDataService {
   public function getTodayViewData($nid) {
     // Get page views for last 24 hours.
     $yesterday = strtotime('-1 day');
-    $query = \Drupal::database()->select('view_count', 'vc');
+    $query = $this->connection->select('view_count', 'vc');
     $query->fields('vc', ['uid', 'timestamp']);
     $query->condition('vc.nid', $nid);
     $query->condition('vc.timestamp', $yesterday, '>');
     $query_result = $query->countQuery()->execute();
-    $this->today_views = $query_result->fetchField();
+    $today_views = $query_result->fetchField();
 
-    return $this->today_views;
+    return $today_views;
   }
 
   /**
@@ -42,14 +45,14 @@ class GetViewDataService {
    */
   public function getLastViewData($nid) {
     // Get all page views.
-    $query = \Drupal::database()->select('view_count', 'vc');
+    $query = $this->connection->select('view_count', 'vc');
     $query->fields('vc', ['uid', 'timestamp']);
     $query->condition('vc.nid', $nid);
     $query->orderBy('timestamp', 'DESC');
     $query->range(0, 1);
-    $this->page_view = $query->execute()->fetchAssoc();
+    $page_view = $query->execute()->fetchAssoc();
 
-    return $this->page_view;
+    return $page_view;
   }
 
   /**
@@ -57,27 +60,23 @@ class GetViewDataService {
    */
   public function getAllViewData($nid) {
     // Get all views for node.
-    $query = \Drupal::database()->select('view_count', 'vc');
+    $query = $this->connection->select('view_count', 'vc');
     $query->fields('vc', ['uid', 'timestamp']);
     $query->condition('vc.nid', $nid);
     $query_result = $query->countQuery()->execute();
-    $this->all_views = $query_result->fetchField();
+    $all_views = $query_result->fetchField();
 
-    return $this->all_views;
+    return $all_views;
   }
 
   /**
    * Write current view to db.
    */
-  public function setViewData($nid) {
-    // Insert data into db, if not ano nymous user.
-    if (!\Drupal::currentUser()->isAnonymous()) {
-      $timestamp = time();
-      $uid = \Drupal::currentUser()->id();
-      $query = \Drupal::database()->insert('view_count')
-        ->fields(array('nid', 'timestamp', 'uid'))
-        ->values([$nid, $timestamp, $uid])
-        ->execute();
-    }
+  public function setViewData($nid, $uid) {
+    $timestamp = time();
+    $query = $this->connection->insert('view_count')
+      ->fields(array('nid', 'timestamp', 'uid'))
+      ->values([$nid, $timestamp, $uid])
+      ->execute();
   }
 }
